ef3e2d0682d2c4fbf5004f9c0210b39e5922fa7a
JordanEJ <91357710+JordanEJ@users.noreply.github.com>
2023-01-14 18:55:25 +1030

    Initial Release

---
+++ esp8266_phising/redirect.h
@@ -0,0 +1,11 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+  <head>
+     <meta http-equiv="refresh" content="5; url='http://172.0.0.1/about'" />
+  </head>
+  <body>
+    <p>If this page does not redirect please <a href="http://172.0.0.1/about">click here.</a>.</p>
+  </body>
+</html>
+)=====";
